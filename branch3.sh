1b4887fb5c65cac95ed676650eed006e7883cf0e
Favour Oyebami <favour.oyebami09@gmail.com>
2026-07-18 15:24:54 +0000

    new conditionals added to API script

--- branch3.sh
+++ branch3.sh
@@ -14,39 +14,38 @@ echo "Base SHA done"
 
 # Create the branch
 echo "Creating branch: $new_branch"
-file_txt=$(curl -s -o /tmp/file_txt -w "%{http_code}" -X POST \
+curl -s -X POST \
   -H "Authorization: Bearer $token" \
   -H "Accept: application/vnd.github+json" \
   https://api.github.com/repos/$owner/$repo/git/refs \
-  -d "{\"ref\":\"refs/heads/$new_branch\",\"sha\":\"$SHA\"}")
+  -d "{\"ref\":\"refs/heads/$new_branch\",\"sha\":\"$SHA\"}"
 
-if [ "$file_txt" -eq 201 ]; then 
-  echo "New branch created: $new_branch"
-else
-echo "New branch failed to create :("
- cat /tmp/file_txt
- exit 1
-fi 
 
 # Validate new branch
 echo "Validating branch: $new_branch"
-curl -s -H "Authorization: Bearer $token" \
-  https://api.github.com/repos/$owner/$repo/branches/$new_branch | jq -r '.name'
-echo "Done with validation"
+created=$(curl -s -H "Authorization: Bearer $token" \
+	https://api.github.com/repos/$owner/$repo/branches/$new_branch | jq -r '.name')
+
+if [[ "$created" -eq "$new_branch" ]]; then 
+   echo "New Branch created successfully and validated."
+else
+    echo "Branch creation failed."
+fi 
 
 # Delete branch
  echo "Deleting newly created branch: $new_branch"
- delete_file_txt=$(curl -s -o /tmp/file_txt -w "%{http_code}" -X DELETE \
+ curl -X DELETE \
   -H "Authorization: Bearer $token" \
   -H "Accept: application/vnd.github+json" \
-  https://api.github.com/repos/$owner/$repo/git/refs/heads/$new_branch)
- 
- if [ "$delete_file_txt" -eq 204 ]; then
-  echo "Branch deleted"
- else 
-  echo "No branch detected"
-  cat /tmp/file_txt
-  exit 1
+  https://api.github.com/repos/$owner/$repo/git/refs/heads/$new_branch
+
+
+# Check if deleted branch still exist
+created=$(curl -s -H "Authorization: Bearer $token" \
+        https://api.github.com/repos/$owner/$repo/branches/$new_branch | jq -r '.name')
+
+if [[ "$created" -eq "null" ]]; then 
+  echo "Branch deleted successfully."
+else
+   echo "Branch deleted failed."
  fi 
-
-
